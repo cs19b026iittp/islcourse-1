2b7ca295c8facf34ac659a3329bccf7522823385
Vivek Mani Charan P <cs19b032@iittp.ac.in>
2022-10-19 09:36:02 +0530

    Update hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,14 +1,34 @@
 import torch
 from torch import nn
-
-def kali():
-  print ('kali')
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
   
-# Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
-class YourRollNumberNN(nn.Module):
-  pass
-  # ... your code ...
-  # ... write init and forward functions appropriately ...
+def charan():
+  print("charan")
+
+# neural network
+class cs19b032NN(nn.Module):
+  def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+  def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square, you can specify with a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
     
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model(train_data_loader=None, n_epochs=10):
