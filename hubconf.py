acbd5bed5db84389bc36e7a0acb900617ca62081
Vivek Mani Charan P <cs19b032@iittp.ac.in>
2022-11-12 11:40:59 +0530

    Update hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,44 +1,55 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
 import torch.nn.functional as F
-from torchvision.transforms import ToTensor
-from torchmetrics.functional import precision_recall
-  
+import numpy as np
+from numpy.lib.function_base import average
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-loss_fn = nn.CrossEntropyLoss()
-
-# neural network
+# Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
 class cs19b032NN(nn.Module):
- def __init__(self, img_size):
-        super(cs19b032NN, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(img_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
+  def __init__(self, config, C, H, W):
+    super(cs19b032NN, self).__init__()
+    self.conv2ds = nn.ModuleList([nn.Conv2d(in_channels, out_channels, kernel, stride, 1) for in_channels, out_channels, kernel, stride, padding in config ])
+    self.m = nn.Softmax(dim=1)
+    for in_channels, out_channels, kernel, stride, padding in config:
+      H = int((H + 2 - kernel[0]) / stride) + 1
+      W = int((W + 2 - kernel[1]) / stride) + 1
+      C = out_channels
+    self.fc = nn.Linear(H*W*C, 10)
+    self.relu = nn.ReLU()
 
- def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-    
+  def forward(self, x):
+    for conv2d in self.conv2ds:
+     x = conv2d(x)
+    x = self.relu(x)
+    x = torch.flatten(x, 1)
+    x = self.fc(x)
+    output = self.m(x)
+    return output
+
+
+def loss_fn(y_pred, y_ground):
+  v = -(y_ground * torch.log(y_pred + 0.0001))
+  v = torch.sum(v)
+  return v
+
+
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
-def get_model(train_data_loader=None, n_epochs=1):
-  
-  dataiter = iter(train_data_loader)
-  img, _ = dataiter.next()
-  
-  model = cs19b032NN(img.shape[2]*img.shape[3]).to(device)
-  optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
+def get_model(train_data_loader=None, n_epochs=10):
+  for X,y in train_data_loader:
+    N, C, H, W =  X.shape
+    break;
+  config = [[C, C+2, (4,4), 1, 'same']]
+  model = cs19b032NN(config, C, H, W).to(device)
   size = len(train_data_loader.dataset)
+  optimizer = torch.optim.SGD(model.parameters(), lr=10e-5)
   model.train()
   for epoch in range(1,n_epochs+1):
     print("Epoch ", epoch);
@@ -47,7 +58,8 @@ def get_model(train_data_loader=None, n_epochs=1):
 
       # Compute prediction error
       pred = model(X)
-      loss = loss_fn(pred, y)
+      y_1h = F.one_hot(y, num_classes= 10)
+      loss = loss_fn(pred, y_1h)
 
       # Backpropagation
       optimizer.zero_grad()
@@ -57,40 +69,45 @@ def get_model(train_data_loader=None, n_epochs=1):
       if batch % 100 == 0:
           loss, current = loss.item(), batch * len(X)
           print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-  
+          
   print ('Returning model... (rollnumber: cs19b032)')
   
   return model
 
+
 # sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
-  
-  
-  model = get_model(train_data_loader, n_epochs)
+  for X,y in train_data_loader:
+    N, C, H, W =  X.shape
+    break;
+  config = [[C, C+2, (4,4), 1, 'same']]
+  model = cs19b032NN(config, C, H, W).to(device)
+  size = len(train_data_loader.dataset)
+  optimizer = torch.optim.SGD(model.parameters(), lr)
+  model.train()
+  for epoch in range(1,n_epochs+1):
+    print("Epoch ", epoch);
+    for batch, (X, y) in enumerate(train_data_loader):
+      X, y = X.to(device), y.to(device)
 
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # Use softmax and cross entropy loss functions
-  # set model variable to proper object, make use of train_data
-  
-  # In addition,
-  # Refer to config dict, where learning rate is given, 
-  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
-  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
-  # You need to create 2d convoution layers as per specification above in each element
-  # You need to add a proper fully connected layer as the last layer
-  
-  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
-  # HINT: Flatten function can also be used if required
-  return model
-  
-  
-  print ('Returning model... (rollnumber: xx)')
+      # Compute prediction error
+      pred = model(X)
+      y_1h = F.one_hot(y, num_classes= 10)
+      loss = loss_fn(pred, y_1h)
+
+      # Backpropagation
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+      if batch % 100 == 0:
+          loss, current = loss.item(), batch * len(X)
+          print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+          
+  print ('Returning model... (rollnumber: cs19b032)')
   
   return model
+
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
 def test_model(model1=None, test_data_loader=None):
@@ -99,27 +116,37 @@ def test_model(model1=None, test_data_loader=None):
   
   size = len(test_data_loader.dataset)
   num_batches = len(test_data_loader)
-  model1.eval()
+  model.eval()
+
+  actual = []   
+  predicted = [] 
   test_loss, correct = 0, 0
-  
-
-  y_true = []
-  y_pred = []
-
   with torch.no_grad():
       for X, y in test_data_loader:
-          y_true.append(y[0])
-          X, y = X.to(device), y.to(device)
-          pred = model1(X)
-          test_loss += loss_fn(pred, y).item()
-          y_pred.append(pred[0].argmax(0))
-          correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+           X, y = X.to(device), y.to(device)
+           y1 = model(X)
+           actual.append(y)
+           predicted.append(y1.argmax(1))
+           y_h = F.one_hot(y, num_classes= 10)
+           test_loss += loss_fn(y1, y_h).item()
+           correct += (y1.argmax(1) == y).type(torch.float).sum().item() 
+        
   test_loss /= num_batches
   correct /= size
+  print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+  predicted = [val.item() for sublist in predicted for val in sublist]
+  actual = [val.item() for sublist in actual for val in sublist]
+
 
   accuracy_val = correct
-  precision_val, recall_val = precision_recall(torch.tensor(y_pred), torch.tensor(y_true), average='macro', num_classes = max(y_pred) + 1)
-  f1score_val = 2*(precision_val * recall_val) / (precision_val + recall_val)
+  precision_val = precision_score(actual, predicted, average='macro')
+  recall_val = recall_score(actual, predicted, average='macro')
+  f1score_val= f1_score(actual, predicted, average='macro')
 
-  print ('Returning metrics... (rollnumber: cs19b032)')
+  # print(f"Accuracy : {(accuracy_val):>0.4f}"),
+  # print(f"Precision: {(precision_val):>0.4f}")
+  # print(f"Recall   : {(recall_val):>0.4f}")
+  # print(f"F1 scores: {(f1score_val):>0.4f}")
+  
   return accuracy_val, precision_val, recall_val, f1score_val
